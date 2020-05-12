26687c0f1860ca0be42167d53a39468544ae2d7e
armanhalizadeh <arman@helloflexy.com>
2020-05-12 16:42:22 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "arman-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://q49muqago9.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_7ydrGAcu1",
+      APP_CLIENT_ID: "683gt98r201s0dk5t9qob0f2qu",
+      IDENTITY_POOL_ID: "us-east-1:ed79b734-5fdb-43f2-b538-9e0c8e261835"
+    }
+  };
